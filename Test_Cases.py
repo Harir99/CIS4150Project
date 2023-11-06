5ac04f2c2227e4ecde4fdd2fde42f635e970d6ca
Rehan Nagoor <rehannags@yahoo.com>
2023-11-06 00:21:30 -0500

    added invalid test case functions

--- Test_Cases.py
+++ Test_Cases.py
@@ -53,3 +53,41 @@ def test_mergesort_dublicates_data():
     input_data = "6 7 2 7 6"
     expected_output = "Sorted list: [2, 6, 6, 7, 7]"
     run_test(input_data, expected_output)
+
+# ---------- INVALID CASES ----------
+
+# No arguments: Test by passing in no arguments
+def test_mergesort_no_arguments():
+    input_data = ""
+    expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
+    run_test(input_data, expected_output)
+
+# String Values: Test with arguments containing only strings
+def test_mergesort_string_values():
+    input_data = "boii wha da heeeee"
+    expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
+    run_test(input_data, expected_output)
+
+# Decimal Values: Test with arguements containing only decimals
+def test_mergesort_decimal_values():
+    input_data = "3.142 0.55 1.12 0.69"
+    expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
+    run_test(input_data, expected_output)
+
+# Some Invalid Values: Test with a arguments containing some valid inputs and different invalid values
+def test_mergesort_invalid_values():
+    input_data = "boii 5 da 3.74 34"
+    expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
+    run_test(input_data, expected_output)
+
+# Values with Whitespace: Test with different types of whitespace
+def test_mergesort_no_arguments():
+    input_data = "3 12   6\n 1\t5"
+    expected_output = "Sorted list: [1, 3, 5, 6, 12]"
+    run_test(input_data, expected_output)
+
+# Values Containing Separators: Test using a value containing comma seperator separators that the program
+def test_mergesort_no_arguments():
+    input_data = "6,23,7,1"
+    expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
+    run_test(input_data, expected_output)
