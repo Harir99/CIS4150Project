340ee2ba29bf73187c8659cee289c2d2590626f6
HazzyK22 <149448150+HazzyK22@users.noreply.github.com>
2023-11-06 21:18:40 -0500

    Update Test_Cases.py

--- Test_Cases.py
+++ Test_Cases.py
@@ -1,4 +1,5 @@
 import subprocess
+import pytest
 
 
 def run_test(input_data, expected_output):
@@ -54,6 +55,43 @@ def test_mergesort_dublicates_data():
     expected_output = "Sorted list: [2, 6, 6, 7, 7]"
     run_test(input_data, expected_output)
 
+# ---------- BOUNDARY TEST CASES ----------
+
+# Test case 12 - Test with lists containing single element (e.g. [6])
+@pytest.mark.Aasim
+def test_mergesort_single_element():
+    input_data = "6"
+    expected_output = "Sorted list: [6]"
+    run_test(input_data, expected_output)
+
+# Test case 13 - Test with lists containing two elements (smallest even) (e.g. [4,2])
+@pytest.mark.Aasim
+def test_mergesort_two_even_elements():
+    input_data = "4 2"
+    expected_output = "Sorted list: [2, 4]"
+    run_test(input_data, expected_output)
+
+# Test case 14 - Test with lists containing three elements (smallest odd) (e.g. [54,8,4])
+@pytest.mark.Aasim
+def test_mergesort_three_odd_elements():
+    input_data = "54 8 4"
+    expected_output = "Sorted list: [4, 8, 54]"
+    run_test(input_data, expected_output)
+
+# Test case 15 - Test with lists containing homogeneous elements (e.g. [6,6,6,6,6])
+@pytest.mark.Aasim
+def test_mergesort_homogeneous_elements():
+    input_data = "6 6 6 6 6"
+    expected_output = "Sorted list: [6, 6, 6, 6, 6]"
+    run_test(input_data, expected_output)
+
+# Test case 16 - Test with lists containing a large number of elements (e.g. [6, 6 … 1000000])
+@pytest.mark.Aasim
+def test_mergesort_large_input_size():
+    input_data = "6 6 6 6 6 5 5 2 0 0 5 7 3 0 10500 10350 9 9 9 8 9 8 8 7 8"
+    expected_output = "Sorted list: [0, 0, 0, 2, 3, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10350, 10500]"
+    run_test(input_data, expected_output)
+
 # ---------- INVALID CASES ----------
 
 # No arguments: Test by passing in no arguments
@@ -68,13 +106,13 @@ def test_mergesort_string_values():
     expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
     run_test(input_data, expected_output)
 
-# Decimal Values: Test with arguements containing only decimals
+# Decimal Values: Test with arguments containing only decimals
 def test_mergesort_decimal_values():
     input_data = "3.142 0.55 1.12 0.69"
     expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
     run_test(input_data, expected_output)
 
-# Some Invalid Values: Test with a arguments containing some valid inputs and different invalid values
+# Some Invalid Values: Test with an argument containing some valid inputs and different invalid values
 def test_mergesort_invalid_values():
     input_data = "boii 5 da 3.74 34"
     expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
@@ -86,7 +124,7 @@ def test_mergesort_whitespace():
     expected_output = "Sorted list: [1, 3, 5, 6, 12]"
     run_test(input_data, expected_output)
 
-# Values Containing Separators: Test using a value containing comma seperator separators that the program
+# Values Containing Separators: Test using a value containing comma separator separators that the program
 def test_mergesort_seperators():
     input_data = "6,23,7,1"
     expected_output = "Usage: python merge_sort.py <space-separated list of numbers>"
